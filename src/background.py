2be885dd6f58eed283cea44ecd11bed3b574729a
Minsoo Kim <ms214@ms214.kr>
2020-11-27 19:08:11 +0900

    update to Kill process and update file.py

--- src/background.py
+++ src/background.py
@@ -4,11 +4,12 @@ import time
 
 from file import File
 from sort import Sort
+from rule import Rule
 
 class Background:
     def __init__(self, path):
         self.from_dir = path
-        self.prefile = [f for f in os.listdir(path) if os.path.isfile(path+f)]
+        self.prefile = [f for f in os.listdir(path) if os.path.isfile(path+f)] # 프로그램 시작 시 파일 리스트
 
     def ckChange(self):
         self.nowfile = [f for f in os.listdir(self.from_dir) if os.path.isfile(self.from_dir+f)]
@@ -17,25 +18,31 @@ class Background:
             if i not in self.prefile:
                 change_file.append(i)
         if len(change_file) != 0:
-            #self.prefile = self.nowfile
-            return True
+            self.prefile = self.nowfile
+            return True # 변경 사항 있음
         else:
-            return False
+            return False # 변경 사항 없음
 
     def bg_move(self):
         r_file = File()
-        rules = r_file.ruleData[1]
+        rules = r_file.ruleData[1] # 수정 해야할 사항.. 유진이 코드에서 규칙 불러오기
         r_sort = Sort(rules, self.from_dir)
         r_sort.ckfile()
         r_sort.move()
-        print("move!")
+
+    def bg_kill(self):
+        try:
+            os.system('taskkill /im pythonw.exe /F')
+        except:
+            pass
 
 
 
 if __name__ == "__main__":
-    # 윈도우에서 실행시 pythonw background.py 형식으로 실행 
+    # 윈도우에서 실행시 pythonw background.py 형식으로 실행
     r_file = File()
     bg = Background(r_file.ruleData[0])
     while True:
-        bg.bg_move()
-        time.sleep(60) # 1분당 한번씩 확인
+        if bg.ckChange():
+            bg.bg_move()
+            # time.sleep(60) # 1분당 한번씩 확인
--- src/file.py
+++ src/file.py
@@ -1,8 +1,11 @@
 import pickle, os
 
+# from-dir이랑 background 유무 확인 정보만 저장하기
+# 나머지는 규칙 저장하는 코드는 유진이가 작성
+
 class File:
     def __init__(self):
-        self.file_name = './data/sort_rule.dat'
+        self.file_name = './data/general.dat'
         self.ruleData = []
         try:
             read = open(self.file_name, 'rb')
@@ -11,32 +14,32 @@ class File:
                 os.mkdir('./data')
             self.ruleData = []
         try:
-            self.ruleData = pickle.load(read) #['From-Dir', [{'id': 'name or id', 'key' : 'keyword', 'value' : 'Hello', 'todir' : 'Directory'}, ]]
+            self.ruleData = pickle.load(read) #['From-Dir', Background]
             read.close()
         except:
             pass
 
-    def writeSortRule(self, fromDir, data): # fromDir은 정렬대상폴더, data는 list형태의 dictionary 모음
+    def writeGeneralRule(self, fromDir, background): # fromDir은 정렬대상폴더, background는 bool 형태(백그라운드 동작 여부 설정)
         read = open(self.file_name, 'wb')
         in_file = [fromDir]
-        in_file.append(data)
+        in_file.append(background) # background는 true or false
         pickle.dump(in_file, read)
         read.close()
 
-    #def modifyRule(self, mod_rule):
-    #    if len(self.ruleData[1]) == len(mod_rule):
-    #        for i in range(len(mod_rule)):
-    #            if self.ruleData[i+1] != mod_rule[i]:
-    #                self.ruleData[i+1] = mod_rule[i]
+    def getGeneralRule(self):
+        try:
+            read = open(self.file_name, 'rb')
+        except FileNotFoundError as fN:
+            pass
+
+        try:
+            return pickle.load(read)
+        except:
+            pass
 
 
 if __name__ == '__main__':
     file = File()
     print(file.ruleData)
-    file.writeSortRule('C:/Users/minso/Downloads/', [{'key': 'keyword', 'value' : 'Hello', 'todir' : 'C:/Users/minso/Desktop/Directory/'}, {'key': 'keyword', 'value' : 'World', 'todir' : 'C:/Users/minso/Desktop/Directory1/'}])
-    file = File()
-    print(file.ruleData)
-    #file.modifyRule([{'key': 'keyword', 'value' : 'welCome', 'todir' : 'Directory'}, {'key': 'pattern', 'value' : 'World', 'todir' : 'Directory'}])
-
-    file = File()
-    print(file.ruleData)
+    file.writeGeneralRule('C:/Users/minso/Downloads/', True)
+    print(file.getGeneralRule())
