5aed3901ee0d2e8d6fc5cd8ed8f999a49aaafd49
Minsoo Kim <ms214@ms214.kr>
2020-11-30 01:29:22 +0900

    규칙저장을 file.py -> rule.py로 이동

--- src/background.py
+++ src/background.py
@@ -1,15 +1,22 @@
 import os
-import sched
+import pickle
 import time
 
 from file import File
 from sort import Sort
-from rule import Rule
+# from rule import Rule
 
 class Background:
     def __init__(self, path):
         self.from_dir = path
         self.prefile = [f for f in os.listdir(path) if os.path.isfile(path+f)] # 프로그램 시작 시 파일 리스트
+        try:
+            rf = open('rule.dat', 'rb')
+            self.names = pickle.load(rf)
+            self.rules = pickle.load(rf) # [[{rule1}], [{rule2}, {rule3}], [{rule4}, {rule5}, {rule6}]]
+            self.toDir = pickle.load(rf)
+        except:
+            pass
 
     def ckChange(self):
         self.nowfile = [f for f in os.listdir(self.from_dir) if os.path.isfile(self.from_dir+f)]
@@ -24,15 +31,13 @@ class Background:
             return False # 변경 사항 없음
 
     def bg_move(self):
-        r_file = File()
-        rules = r_file.ruleData[1] # 수정 해야할 사항.. 유진이 코드에서 규칙 불러오기
-        r_sort = Sort(rules, self.from_dir)
+        r_sort = Sort(self.names, self.rules, self.from_dir, self.toDir)
         r_sort.ckfile()
         r_sort.move()
 
-    def bg_kill(self):
+    def bg_kill(self): # mainWindow 에서 자동분류 체크박스를 해제하였으면 실행 or 이 코드를 mainWindow로 가져가서 실행
         try:
-            os.system('taskkill /im pythonw.exe /F')
+            os.system('taskkill /im pythonw.exe /F') # 윈도우 cmd 명령어
         except:
             pass
 
--- src/sort.py
+++ src/sort.py
@@ -3,10 +3,14 @@ import shutil
 import re
 import platform
 
+# 선택한 규칙과 선택하지 않은 규칙을 어떻게 분류하지?
+
 class Sort:
-    def __init__(self, rule, from_dir):
-        self.rule = rule # dictionary in list key = keyword, pattern... ex)[{key: keyword, value: 'hello', todir: directory}] -> keyword가 'hello'인 파일을 directory로 이동
+    def __init__(self, id, rule, from_dir, toDir):
+        self.ids = id
+        self.rule = rule # [[{rule1}], [{rule2}, {rule3}], [{rule4}, {rule5}, {rule6}]]
         self.from_dir = from_dir
+        self.toDir = toDir
         self.files = [f for f in os.listdir(from_dir) if os.path.isfile(from_dir+f)] # 해당 디렉토리에 있는 모든 파일 목록 저장
         self.s_file = [] # 이동 대상 파일 각요소는 (파일명.확장자, 이동위치) 형식
 
@@ -16,30 +20,29 @@ class Sort:
                 try:
                     shutil.move(self.from_dir+i[0], i[1]+i[0])
                 except:
-                    if 'Linux' in platform.platform() : # 실행 환경이 linux일때
-                        os.chmod(self.from_dir+i[0], 777) # 파일의 권한 변경 (owner, group, other 모두 read, write, execute 권한 부여)
+                    pass
             else:
                 os.mkdir(i[1])
                 try:
                     shutil.move(self.from_dir + i[0], i[1] + i[0])
                 except:
-                    if 'Linux' in platform.platform() : # 실행 환경이 linux일때
-                        os.chmod(self.from_dir+i[0], 777) # 파일의 권한 변경 (owner, group, other 모두 read, write, execute 권한 부여)
+                    pass
 
             
     def ckfile(self):
-        for r in self.rule:
-            for f in self.files:
-                if r['key'] == 'keyword': # key가 keyword
-                    if r['value'].lower() in f.split('.')[0].lower(): # 대소문자 구별 없게 하기 위해서 upper()함수 사용
-                        self.s_file.append((f, r['todir']))
-                elif r['key'] == 'pattern': # key가 pattern 일때
-                    p = re.compile(r['value'])
-                    if p.match(f.split('.')[0]) != None: # 정규식사용, 파일명이 정규식과 맞지 않으면 None 리턴
-                        self.s_file.append((f, r['todir']))
-                elif r['key'] == 'extension': # key가 확장자일때
-                    if r['value'] == f.split('.')[1]:
-                        self.s_file.append((f, r['todir']))
+        for r1 in range(len(self.rule)):
+            for r in self.rule[r1]:
+                for f in self.files:
+                    if r['key'] == 'titleKey': # key가 keyword
+                        if r['value'].lower() in f.split('.')[0].lower(): # 대소문자 구별 없게 하기 위해서 upper()함수 사용
+                            self.s_file.append((f, self.toDir[r1]))
+                    elif r['key'] == 'pattern': # key가 pattern 일때
+                        p = re.compile(r['value'])
+                        if p.match(f.split('.')[0]) != None: # 정규식사용, 파일명이 정규식과 맞지 않으면 None 리턴
+                            self.s_file.append((f, self.toDir[r1]))
+                    elif r['key'] == 'extension': # key가 확장자일때
+                        if r['value'] == f.split('.')[1]:
+                            self.s_file.append((f, self.toDir[r1]))
 
 #테스트를 위한 코드
 if __name__ == "__main__":
