bfad970439b3c45dccbdcd7723606822bcb370e7
Minsoo Kim <ms214@ms214.kr>
2020-12-08 20:22:39 +0900

    self.fromDir이 잘못된 값을 불러오는 버그 수정 && 대상폴더이름+파일이름으로 되어 이동되는 버그 수정

--- src/sort.py
+++ src/sort.py
@@ -12,8 +12,8 @@ class Sort:
         f = open('rule.dat', 'rb')
         self.rule = []
         self.toDir = []
+        ids = pickle.load(f)
         rules = pickle.load(f) # [[{rule1}], [{rule2}, {rule3}], [{rule4}, {rule5}, {rule6}]]
-        self.from_dir = pickle.load(f)
         toDirs = pickle.load(f)
         f.close()
         f = open('checked_idx.dat', 'rb')
@@ -21,6 +21,10 @@ class Sort:
         for i in selNums:
             self.rule.append(rules[i])
             self.toDir.append(toDirs[i])
+        f.close()
+        f = open('general.dat', 'rb')
+        loads = pickle.load(f)
+        self.from_dir = loads[0]
         f.close()
         self.files = [f for f in os.listdir(self.from_dir) if os.path.isfile(self.from_dir+f)] # 해당 디렉토리에 있는 모든 파일 목록 저장
         self.s_file = [] # 이동 대상 파일 각요소는 (파일명.확장자, 이동위치) 형식
@@ -46,11 +50,11 @@ class Sort:
                 for f in self.files:
                     if r['key'] == 'titleKey': # key가 keyword
                         if r['value'].lower() in f.split('.')[0].lower(): # 대소문자 구별 없게 하기 위해서 upper()함수 사용
-                            self.s_file.append((f, self.toDir[r1]))
+                            self.s_file.append((f, self.toDir[r1])) if self.toDir[r1][-1] == '/' else self.s_file.append((f, self.toDir[r1]+'/'))
                     elif r['key'] == 'titlePattern': # key가 pattern 일때
                         p = re.compile(r['value'])
                         if p.match(f.split('.')[0]) != None: # 정규식사용, 파일명이 정규식과 맞지 않으면 None 리턴
-                            self.s_file.append((f, self.toDir[r1]))
+                            self.s_file.append((f, self.toDir[r1])) if self.toDir[r1][-1] == '/' else self.s_file.append((f, self.toDir[r1]+'/'))
                     elif r['key'] == 'fileFormat': # key가 확장자일때
                         if r['value'] == f.split('.')[1]:
                             self.s_file.append((f, self.toDir[r1]))
@@ -61,7 +65,7 @@ class Sort:
                             res = res.split('\n')[3]
                             res = res.split('=', maxsplit=1)[2]
                             if res == r['value']:
-                                self.s_file.append((f, self.toDir[r1]))
+                                self.s_file.append((f, self.toDir[r1])) if self.toDir[r1][-1] == '/' else self.s_file.append((f, self.toDir[r1]+'/'))
 
 
 
